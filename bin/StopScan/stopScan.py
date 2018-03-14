23ab4a4c7559def046d7c9ebb32b4e0a08d1cea2
gnjavie@3p.mapfre.com <jgutierrez@localhost.localdomain>
2018-03-14 10:43:11 +0100

    Creada la SDK de Nessus y un script para parar los scaneres de una folder de nessus

---
+++ bin/StopScan/stopScan.py
@@ -0,0 +1,64 @@
+import click
+import configparser
+import codecs
+import sys
+import os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../../src/NessusApi')
+from time import gmtime, strftime
+from nessusRest import Scanner
+
+
+@click.command()
+@click.option('--folder_id', '-f', help="Id of the folder to stop")
+#@click.option('--config_file', '-c', default="", type=(str), help="Config file with the nessus info")
+def stop_scan(folder_id):
+    scanner = getScanner()
+
+    scan_list = scanner.scan_list_from_folder(folder_id)
+    actual_time = strftime("%d/%m/%Y %H:%M:%S", gmtime())
+    print("[{}] Checking the folder {} with #{} scans:".format(
+        actual_time, folder_id, len(scan_list)))
+
+    for scan in scan_list:
+        if str(scan[u'status']) == "running":
+            print("  - [!] Running Scan [#{}] {}. Stopping...".format(
+                scan[u'id'], scan[u'name']))
+            scanner.scan_stop(scan[u'id'])
+            print("  ....SCAN STOPPED")
+        else:
+            print("  - Scan [#{}] {}: STATUS = {}".format(
+                scan[u'status'], scan[u'id'], scan[u'name']))
+
+    print("-----")
+
+def setProxy(host):
+    no_proxy = os.environ.get('NO_PROXY', '')
+
+    if no_proxy == '':
+        no_proxy = "127.0.0.1,localhost,%s" % host
+    elif host not in no_proxy:
+        no_proxy += ",%s" % host
+
+    os.environ['NO_PROXY'] = no_proxy
+    os.environ['HTTP_PROXY'] = ''
+    os.environ['HTTPS_PROXY'] = ''
+
+def getScanner(config_file=""):
+    if config_file == '':
+        config_file = os.path.join(myPath, 'config', 'nessus.ini')
+
+    config = configparser.ConfigParser()
+    config.readfp(codecs.open(config_file, "r"))
+
+    setProxy("10.139.90.81")
+    host = config['nessus'].get('url', 'https://127.0.0.1:8834')
+    login = config['nessus'].get('login', 'admin')
+    password = config['nessus'].get('password', '1234')
+    insecure = config['nessus'].get('insecure', 'true') in ['true', 'TRUE', 1, '1']
+    scanner = Scanner(url=host, login=login, password=password, insecure=insecure, bypass_proxy=True)
+    return scanner
+
+
+if __name__ == "__main__":
+    stop_scan()
